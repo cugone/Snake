8f1f85c4d76e8bf6cc359948f2439940144a1bf6
cugone <cugone@gmail.com>
2024-05-19 15:36:14 -0500

    Add .hlsl and .cso files to project

---
+++ Snake/Run_x64/Data/ShaderPrograms/tile.hlsl
@@ -0,0 +1,53 @@
+
+cbuffer matrix_cb : register(b0) {
+    float4x4 g_MODEL;
+    float4x4 g_VIEW;
+    float4x4 g_PROJECTION;
+};
+
+cbuffer time_cb : register(b1) {
+    float g_GAME_TIME;
+    float g_SYSTEM_TIME;
+    float g_GAME_FRAME_TIME;
+    float g_SYSTEM_FRAME_TIME;
+}
+
+cbuffer wall_cb : register(b3) {
+    float4 g_TILESELECTOR_PADDING3;
+}
+
+struct vs_in_t {
+    float3 position : POSITION;
+    float4 color : COLOR;
+    float2 uv : UV;
+};
+
+struct ps_in_t {
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+    float2 uv : UV;
+};
+
+SamplerState sSampler : register(s0);
+
+Texture2DArray<float4> tTile : register(t0);
+
+ps_in_t VertexFunction(vs_in_t input_vertex) {
+    ps_in_t output;
+
+    float4 local = float4(input_vertex.position, 1.0f);
+    float4 world = mul(local, g_MODEL);
+    float4 view = mul(world, g_VIEW);
+    float4 clip = mul(view, g_PROJECTION);
+
+    output.position = clip;
+    output.color = input_vertex.color;
+    output.uv = input_vertex.uv;
+
+    return output;
+}
+
+float4 PixelFunction(ps_in_t input_pixel) : SV_Target0 {
+    float4 albedo = tTile.Sample(sSampler, float3(input_pixel.uv, g_TILESELECTOR_PADDING3.x) );
+    return albedo * input_pixel.color;
+}
