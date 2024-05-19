d3c07913ffa1962293f0fb0cc415f685c87abc74
cugone <cugone@gmail.com>
2024-05-19 15:37:08 -0500

    Implement tile rendering on GPU

--- Snake/Code/Game/Wall.cpp
+++ Snake/Code/Game/Wall.cpp
@@ -1,50 +1,95 @@
 #include "Game/Wall.hpp"
 
-#include "Engine/Math/AABB2.hpp"
 #include "Engine/Core/EngineCommon.hpp"
 
-#include "Engine/Renderer/Renderer.hpp"
+#include "Engine/Services/ServiceLocator.hpp"
+#include "Engine/Services/IRendererService.hpp"
 
+#include "Engine/Math/AABB2.hpp"
+#include "Engine/Math/MathUtils.hpp"
+
+#include "Engine/Renderer/Renderer.hpp"
+#include "Engine/Renderer/ConstantBuffer.hpp"
+
+constexpr static float wall_index_offset = 12.0f;
+
+constexpr float WallDirectionToTextureIndexOffset(Wall::Direction direction) noexcept;
+Wall::Direction GetRandomDirection() noexcept;
+
+constexpr float WallDirectionToTextureIndexOffset(Wall::Direction direction) noexcept {
+    switch (direction) {
+    case Wall::Direction::Full:
+        return wall_index_offset + 0.0f;
+    case Wall::Direction::Left:
+        return wall_index_offset + 1.0f;
+    case Wall::Direction::Horizontal:
+        return wall_index_offset + 2.0f;
+    case Wall::Direction::Right:
+        return wall_index_offset + 3.0f;
+    case Wall::Direction::Up:
+        return wall_index_offset + 4.0f;
+    case Wall::Direction::Vertical:
+        return wall_index_offset + 5.0f;
+    case Wall::Direction::Down:
+        return wall_index_offset + 6.0f;
+    default:
+        return wall_index_offset;
+    }
+}
+
+Wall::Direction GetRandomDirection() noexcept {
+    return static_cast<Wall::Direction>(MathUtils::GetRandomLessThan(7));
+}
+
+Wall::Wall(const Vector2& position, const Wall::Direction& direction) noexcept 
+: position(position)
+, direction(direction)
+{
+    auto* r = ServiceLocator::get<IRendererService>();
+    if (const auto& cbs = r->GetMaterial("tile")->GetShader()->GetConstantBuffers(); !cbs.empty()) {
+        selection_state_cb = &cbs[0].get();
+    }
+    this->direction = static_cast<Wall::Direction>(GetRandomDirection());
+}
 
 void Wall::BeginFrame() noexcept {
-    builder.Clear();
+    /* DO NOTHING */
 }
 
 void Wall::Update([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept {
+    /* DO NOTHING */
+}
 
-    const auto uvs = AABB2::Zero_to_One;
-    const auto mat = g_theRenderer->GetMaterial("wall");
+void Wall::Render() const {
+    selection_state.direction_padding3.x = WallDirectionToTextureIndexOffset(direction);
+    selection_state_cb->Update(*g_theRenderer->GetDeviceContext(), &selection_state);
+}
+
+void Wall::EndFrame() noexcept {
+    /* DO NOTHING */
+}
+
+void Wall::AppendToMesh(Mesh::Builder& builder) noexcept {
 
     builder.Begin(PrimitiveType::Triangles);
 
     builder.SetColor(Rgba::White);
 
+    const auto uvs = AABB2::Zero_to_One;
     builder.SetUV(Vector2{ uvs.mins.x, uvs.maxs.y });
-    builder.AddVertex(Vector2{ -0.5f, +0.5f });
+    builder.AddVertex(position + Vector2{ -0.5f, +0.5f });
 
     builder.SetUV(Vector2{ uvs.mins.x, uvs.mins.y });
-    builder.AddVertex(Vector2{ -0.5f, -0.5f });
+    builder.AddVertex(position + Vector2{ -0.5f, -0.5f });
 
     builder.SetUV(Vector2{ uvs.maxs.x, uvs.mins.y });
-    builder.AddVertex(Vector2{ +0.5f, -0.5f });
+    builder.AddVertex(position + Vector2{ +0.5f, -0.5f });
 
     builder.SetUV(Vector2{ uvs.maxs.x, uvs.maxs.y });
-    builder.AddVertex(Vector2{ +0.5f, +0.5f });
-
+    builder.AddVertex(position + Vector2{ +0.5f, +0.5f });
 
     builder.AddIndicies(Mesh::Builder::Primitive::Quad);
-    builder.End(mat);
-}
 
-void Wall::Render() const {
-    const auto S = Matrix4::CreateScaleMatrix(Vector2::One * 32.0f);
-    const auto R = Matrix4::I;
-    auto T = Matrix4::I;
-    auto M = Matrix4::MakeSRT(S, R, T);
-    g_theRenderer->SetModelMatrix(M);
-    Mesh::Render(builder);
-}
-
-void Wall::EndFrame() noexcept {
+    builder.End(g_theRenderer->GetMaterial("tile"));
 
 }
--- Snake/Code/Game/Wall.hpp
+++ Snake/Code/Game/Wall.hpp
@@ -2,15 +2,47 @@
 
 #include "Engine/Core/TimeUtils.hpp"
 
+#include "Engine/Math/Vector2.hpp"
+
 #include "Engine/Renderer/Mesh.hpp"
+
+class ConstantBuffer;
 
 class Wall {
 public:
+    enum class Direction : int {
+        Full,
+        Left,
+        Horizontal,
+        Right,
+        Up,
+        Vertical,
+        Down,
+    };
+    Wall() = default;
+    Wall(const Vector2& position, const Direction& direction) noexcept;
+    Wall(const Wall& other) = default;
+    Wall(Wall&& rother) = default;
+    Wall& operator=(const Wall& other) = default;
+    Wall& operator=(Wall&& rother) = default;
+    ~Wall() = default;
+
     void BeginFrame() noexcept;
     void Update(TimeUtils::FPSeconds deltaSeconds) noexcept;
     void Render() const;
     void EndFrame() noexcept;
+
+    void AppendToMesh(Mesh::Builder& builder) noexcept;
+
+    Vector2 position{};
+    Direction direction{Direction::Full};
 protected:
 private:
-    Mesh::Builder builder{};
+
+    struct selection_state_t {
+        Vector4 direction_padding3;
+    };
+
+    ConstantBuffer* selection_state_cb{ nullptr };
+    mutable selection_state_t selection_state{};
 };
